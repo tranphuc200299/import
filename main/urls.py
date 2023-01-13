ee8f26f4cc9109dd9d423927429cd0b1a2f3755c
hainv-ntq <hai.nguyen2@ntq-solution.com.ntq>
2023-01-13 11:09:15 +0700

    dev feature

--- main/urls.py
+++ main/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("f_cfsc0300/", views.menu4.f_cfsc0300, name="f_cfsc0300"),
     path("f_cfsc0100/", views.menu4.f_cfsc0100, name="f_cfsc0100"),
     path("f_cfsc0500/", views.menu4.f_cfsc0500, name="f_cfsc0500"),
+    path("f_cfsc2700/", views.menu4.f_cfsc2700, name="f_cfsc2700"),
 
 ]
